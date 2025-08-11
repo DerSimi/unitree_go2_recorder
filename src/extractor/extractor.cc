02d77ecbeaf2de14dbf27d420e137d52a553f14c
DerSimi <simi2657@gmail.com>
2025-08-11 16:07:00 +0200

    added buffer handling

--- src/extractor/extractor.cc
+++ src/extractor/extractor.cc
@@ -8,7 +8,7 @@ MujocoExtractor::MujocoExtractor(mjModel *model, mjData *data) : mj_model_(model
 {
     CheckSensor();
     low_cmd_suber_.reset(new ChannelSubscriber<unitree_go::msg::dds_::LowCmd_>(TOPIC_LOWCMD));
-    low_cmd_suber_->InitChannel(bind(&MujocoExtractor::LowCmdGoHandler, this, placeholders::_1), 1);
+    low_cmd_suber_->InitChannel(bind(&MujocoExtractor::LowCmdHandler, this, placeholders::_1), 1);
 
     low_state_suber_.reset(new ChannelSubscriber<unitree_go::msg::dds_::LowState_>(TOPIC_LOWSTATE));
     low_state_suber_->InitChannel(bind(&MujocoExtractor::LowStateHandler, this, placeholders::_1), 1);
@@ -17,17 +17,36 @@ MujocoExtractor::MujocoExtractor(mjModel *model, mjData *data) : mj_model_(model
     high_state_suber_->InitChannel(bind(&MujocoExtractor::HighStateHandler, this, placeholders::_1), 1);
 }
 
-void MujocoExtractor::LowCmdGoHandler(const void *msg)
+void MujocoExtractor::LowCmdHandler(const void *msg)
 {
+    auto now = std::chrono::system_clock::now();
+    double timestamp = std::chrono::duration<double>(now.time_since_epoch()).count();
+
     const unitree_go::msg::dds_::LowCmd_ *cmd = (const unitree_go::msg::dds_::LowCmd_ *)msg;
     if (mj_data_)
     {
+        LowCmdData data;
+        data.timestamp = timestamp;
+        data.ctrl.resize(num_motor_);
+
         for (int i = 0; i < num_motor_; i++)
         {
             mj_data_->ctrl[i] = cmd->motor_cmd()[i].tau() +
                                 cmd->motor_cmd()[i].kp() * (cmd->motor_cmd()[i].q() - mj_data_->sensordata[i]) +
                                 cmd->motor_cmd()[i].kd() * (cmd->motor_cmd()[i].dq() - mj_data_->sensordata[i + num_motor_]);
+            data.ctrl[i] = mj_data_->ctrl[i];
         }
+
+        // Lock mutex and write into buffer
+        {
+            std::lock_guard<std::mutex> lock(mtx_);
+            low_cmd_buffer_.push_back(data);
+
+            if (low_cmd_buffer_.size() > SYNC_BUFFER_MAX_SIZE)
+            {
+                low_cmd_buffer_.pop_front();
+            }
+        } // Free mutex
     }
 }
 
@@ -49,10 +68,16 @@ wir haben 12 gelenke, pro gelenk eine Geschwindigkeit, 6 + 12 = 18
 
 void MujocoExtractor::LowStateHandler(const void *msg)
 {
+    auto now = std::chrono::system_clock::now();
+    double timestamp = std::chrono::duration<double>(now.time_since_epoch()).count();
+
     const unitree_go::msg::dds_::LowState_ *state = (const unitree_go::msg::dds_::LowState_ *)msg;
 
     if (mj_data_)
     {
+        LowStateData data;
+        data.timestamp = timestamp;
+
         // Inject IMU state
         if (have_frame_sensor_)
         {
@@ -61,41 +86,93 @@ void MujocoExtractor::LowStateHandler(const void *msg)
             mj_data_->qpos[4] = state->imu_state().quaternion()[1];
             mj_data_->qpos[5] = state->imu_state().quaternion()[2];
             mj_data_->qpos[6] = state->imu_state().quaternion()[3];
+
+            data.base_quat[0] = mj_data_->qpos[3];
+            data.base_quat[1] = mj_data_->qpos[4];
+            data.base_quat[2] = mj_data_->qpos[5];
+            data.base_quat[3] = mj_data_->qpos[6];
+
             // Angular velocity
             mj_data_->qvel[0] = state->imu_state().gyroscope()[0];
             mj_data_->qvel[1] = state->imu_state().gyroscope()[1];
             mj_data_->qvel[2] = state->imu_state().gyroscope()[2];
 
+            data.base_ang_vel[0] = mj_data_->qvel[0];
+            data.base_ang_vel[1] = mj_data_->qvel[1];
+            data.base_ang_vel[2] = mj_data_->qvel[2];
+
             // Linear velocity is not in here, only acceleration!!!
         }
+
+        data.qpos_joints.resize(num_motor_);
+        data.qvel_joints.resize(num_motor_);
 
         // Inject motor state
         for (int i = 0; i < num_motor_; i++)
         {
             // angle
             mj_data_->qpos[7 + i] = state->motor_state()[i].q();
+            data.qpos_joints[i] = mj_data_->qpos[7 + i];
+
             // angular velocity
             mj_data_->qvel[7 + i] = state->motor_state()[i].dq();
+            data.qvel_joints[i] = mj_data_->qvel[7 + i];
         }
+
+        // Lock mutex and write into buffer
+        {
+            std::lock_guard<std::mutex> lock(mtx_);
+            low_state_buffer_.push_back(data);
+
+            if (low_state_buffer_.size() > SYNC_BUFFER_MAX_SIZE)
+            {
+                low_state_buffer_.pop_front();
+            }
+        } // Free mutex
     }
 }
 
 void MujocoExtractor::HighStateHandler(const void *msg)
 {
+    auto now = std::chrono::system_clock::now();
+    double timestamp = std::chrono::duration<double>(now.time_since_epoch()).count();
+
     const unitree_go::msg::dds_::SportModeState_ *state = (const unitree_go::msg::dds_::SportModeState_ *)msg;
 
     // Inject position and velocity into mujoco data
     if (mj_data_ && have_frame_sensor_)
     {
+        HighStateData data;
+        data.timestamp = timestamp;
+
         // position
         mj_data_->qpos[0] = state->position()[0];
         mj_data_->qpos[1] = state->position()[1];
         mj_data_->qpos[2] = state->position()[2];
 
+        data.base_pos[0] = mj_data_->qpos[0];
+        data.base_pos[1] = mj_data_->qpos[1];
+        data.base_pos[2] = mj_data_->qpos[2];
+
         // velocity
         mj_data_->qvel[0] = state->velocity()[0];
         mj_data_->qvel[1] = state->velocity()[1];
         mj_data_->qvel[2] = state->velocity()[2];
+
+        data.base_lin_vel[0] = mj_data_->qvel[0];
+        data.base_lin_vel[1] = mj_data_->qvel[1];
+        data.base_lin_vel[2] = mj_data_->qvel[2];
+
+        // Lock mutex and write into buffer
+        {
+            std::lock_guard<std::mutex> lock(mtx_);
+            high_state_buffer_.push_back(data);
+
+            if (high_state_buffer_.size() > SYNC_BUFFER_MAX_SIZE)
+            {
+                high_state_buffer_.pop_front();
+            }
+        } // Free mutex
     }
 }
 
--- src/extractor/extractor.h
+++ src/extractor/extractor.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <deque>
+#include <chrono>
 
 #include <unitree/robot/channel/channel_subscriber.hpp>
 #include <unitree/idl/go2/SportModeState_.hpp>
@@ -17,6 +19,26 @@ using namespace std;
 #define TOPIC_HIGHSTATE "rt/sportmodestate"
 #define TOPIC_LOWCMD "rt/lowcmd"
 #define MOTOR_SENSOR_NUM 3
+#define SYNC_BUFFER_MAX_SIZE 100
+
+struct LowStateData {
+    double timestamp;
+    std::vector<mjtNum> qpos_joints; // 12 joint angles
+    std::vector<mjtNum> qvel_joints; // 12 joint velocities
+    mjtNum base_quat[4];             // base rotation
+    mjtNum base_ang_vel[3];          // base angular velocity
+};
+
+struct HighStateData {
+    double timestamp;
+    mjtNum base_pos[3];     // base position
+    mjtNum base_lin_vel[3]; // base linear velocity
+};
+
+struct LowCmdData {
+    double timestamp;
+    std::vector<mjtNum> ctrl; // 12 control commands
+};
 
 class MujocoExtractor
 {
@@ -24,7 +46,7 @@ public:
     MujocoExtractor(mjModel *model, mjData *data);
     ~MujocoExtractor();
 
-    void LowCmdGoHandler(const void *msg);
+    void LowCmdHandler(const void *msg);
     void LowStateHandler(const void *msg);
     void HighStateHandler(const void *msg);
 
@@ -36,6 +58,12 @@ private:
     ChannelSubscriberPtr<unitree_go::msg::dds_::LowCmd_> low_cmd_suber_;
     ChannelSubscriberPtr<unitree_go::msg::dds_::LowState_> low_state_suber_;
     ChannelSubscriberPtr<unitree_go::msg::dds_::SportModeState_> high_state_suber_;
+
+    std::deque<LowStateData> low_state_buffer_;
+    std::deque<HighStateData> high_state_buffer_;
+    std::deque<LowCmdData> low_cmd_buffer_;
+
+    std::mutex mtx_;
 
     mjData *mj_data_;
     mjModel *mj_model_;
